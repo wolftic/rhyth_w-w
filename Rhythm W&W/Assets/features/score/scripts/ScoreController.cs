87cd4f5cb32dcc3a833afe5f1f9922b29764862d
Casper Heringa <5be8141b@opayq.com>
2019-05-21 10:59:21 +0200

    added score system and score display

---
+++ Rhythm W&W/Assets/features/score/scripts/ScoreController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ScoreController : Singleton<ScoreController>
+{
+    public int scoreCount = 0;
+
+    public event System.Action<int> OnScoreChanged;
+
+
+    private void Start()
+    {
+        CollectableController.Instance.OnPickup += OnPickup;
+    }
+
+    private void OnPickup(int score)
+    {
+        scoreCount += score;
+        if (OnScoreChanged != null) OnScoreChanged(scoreCount);
+    }
+
+    private void Update()
+    {
+
+    }
+
+}
---
+++ Rhythm W&W/Assets/features/score/scripts/ScoreDisplay.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreDisplay : MonoBehaviour
+{
+    public Text scoreText;
+
+    void Start()
+    {
+        ScoreController.Instance.OnScoreChanged += OnScoreChanged;
+    }
+
+    void OnScoreChanged(int scoreCount)
+    {
+        scoreText.text = "Score: " + scoreCount.ToString();
+    }
+}
