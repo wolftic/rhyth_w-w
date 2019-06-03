549a6f63bb8346e6118424c8a7dfa691158581b0
Kaj <kaaiman99@hotmail.com>
2019-06-03 21:07:55 +0200

    Added Game over/Score pop up screen

---
+++ Rhythm W&W/Assets/GamePopUpPrefab.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GamePopUpPrefab : MonoBehaviour {
+
+    public Action<string> ButtonPressed;
+
+    private Text _levelName;
+    private Text _scoreAmount;
+    
+	void Awake () {
+        _levelName = transform.Find("bg/lvl").GetComponent<Text>();
+        _scoreAmount = transform.Find("bg/scoreamount").GetComponent<Text>();
+	}
+	
+    public void Set(string levelName, int playerScore, int maxScore)
+    {
+        _levelName.text = levelName;
+        _scoreAmount.text = playerScore + "/" + maxScore;
+    }
+
+    public void OnButtonPressed(string buttonName) // de twee button namen zijn "retry" "continue"
+    {
+        ButtonPressed(buttonName);
+    }
+}
