7a28502ab3c306ab93a693cb4efc5076733739f5
Casper Heringa <5be8141b@opayq.com>
2019-06-06 09:51:50 +0200

    implemented pause screen

---
+++ Rhythm W&W/Assets/features/pause/scripts/PauseMenu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _pauseButton;
+    [SerializeField]
+    private GameObject _playButton;
+    [SerializeField]
+    private GameObject _retryButton;
+    [SerializeField]
+    private GameObject _returnToMenuButton;
+
+
+    public void Pause()
+    {
+        Time.timeScale = 0;
+        _pauseButton.SetActive(false);
+
+        _playButton.SetActive(true);
+        _retryButton.SetActive(true);
+        _returnToMenuButton.SetActive(false);
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+
+        _playButton.SetActive(false);
+        _retryButton.SetActive(false);
+        _returnToMenuButton.SetActive(false);
+
+        _pauseButton.SetActive(true);
+    }
+
+}
