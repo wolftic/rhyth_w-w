8fc9b88a0f11c46680fb967c3d95308d6f1e359f
Casper Heringa <5be8141b@opayq.com>
2019-06-06 10:05:03 +0200

    cleaned code and created pause screen prefab

--- Rhythm W&W/Assets/features/pause/scripts/PauseMenu.cs
+++ Rhythm W&W/Assets/features/pause/scripts/PauseMenu.cs
@@ -7,31 +7,19 @@ public class PauseMenu : MonoBehaviour
     [SerializeField]
     private GameObject _pauseButton;
     [SerializeField]
-    private GameObject _playButton;
-    [SerializeField]
-    private GameObject _retryButton;
-    [SerializeField]
-    private GameObject _returnToMenuButton;
-
+    private GameObject _pauseScreen;
 
     public void Pause()
     {
         Time.timeScale = 0;
         _pauseButton.SetActive(false);
-
-        _playButton.SetActive(true);
-        _retryButton.SetActive(true);
-        _returnToMenuButton.SetActive(false);
+        _pauseScreen.SetActive(true);
     }
 
     public void ResumeGame()
     {
         Time.timeScale = 1;
-
-        _playButton.SetActive(false);
-        _retryButton.SetActive(false);
-        _returnToMenuButton.SetActive(false);
-
+        _pauseScreen.SetActive(false);
         _pauseButton.SetActive(true);
     }
 
